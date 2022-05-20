355c1182f43c4f63629b7a606de0b77c0adea4c0
AshikSazal <sojolislm@gmail.com>
2022-05-20 16:48:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 import "./App.css";
 
 const App = () => {
-  const courseGoals = [
+  // useState return two elements
+  // 1st element is latest state snapshot
+  // 2nd element is function that allows to updata that state snapshot
+  const [courseGoals, setCourseGoals] = useState([
     { id: "cg1", text: "Finish the Course" },
     { id: "cg2", text: "Learn all about the Course Main Topic" },
     { id: "cg3", text: "Help other students in the Course Q&amp;A" },
-];
+  ]);
 
-const addNewGoalHandler = (newGoal) =>{
-  courseGoals.push(newGoal);
-  // console.log(courseGoals);
-};
+  const addNewGoalHandler = (newGoal) => {
+    // first approach only when not depends on previous update data
+    // setCourseGoals(courseGoals.concat(newGoal)); 
+
+    // second approach only needed your state update depends on previous state's data
+    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+  };
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal onAddGoal = {addNewGoalHandler} />
-      <GoalList goals = {courseGoals} />
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
