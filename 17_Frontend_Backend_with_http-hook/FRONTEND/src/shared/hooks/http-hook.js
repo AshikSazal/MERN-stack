6e2c3f98eebe21fc66fd1747503fbae493201b71
Ashik Sazal <sojolislm@gmail.com>
2023-01-08 14:58:36 +0600

    first commit

---
+++ 17_Frontend_Backend_with_http-hook/FRONTEND/src/shared/hooks/http-hook.js
@@ -0,0 +1,60 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  // for switching the page very first there may be an error occurred that's why cancel the http request
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+
+      // cancel the http request
+      // AbortController() api work in modern browser
+      const httpAbortController = new AbortController();
+      activeHttpRequests.current.push(httpAbortController);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortController.signal,
+        });
+        const responseData = await response.json();
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortController
+        );
+
+        // error 400 and 500
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  // cancel the http request
+  // clean up function
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
